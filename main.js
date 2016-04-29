f865fa9f192b7846823aa01ae53a476717c59073
David <david@davidjh.co.uk>
2016-04-29 13:16:41 +0100

    CHange function about

--- main.js
+++ main.js
@@ -16,15 +16,18 @@ document.getElementById("clearing-form__field__button").addEventListener("click"
 myDataRef.on('child_added', function(snapshot) {
   var message = snapshot.val();
   var key = snapshot.key();
-  displayChatMessage(message.name, message.grade, message.places, key);
+  displayOpportunity(message.name, message.grade, message.places, key);
 });
 
-function displayChatMessage(name, grade, places, key) {
-  var messageDiv = document.getElementById('clearing-opportunities');
-  messageDiv.innerHTML = messageDiv.innerHTML + `<div class="clearing-opportunities__opportunity">${name} - ${grade} - (${places})</div>`;
-}
+
 
 document.getElementById("deleteItem").addEventListener("click", function(){
   var reftoDelete = new Firebase('https://ce5csxm61gl.firebaseio-demo.com/-KGSH2igrcw_uv9IdPRC');
   reftoDelete.remove();
 });
+
+function displayOpportunity(name, grade, places, key) {
+  var opportunitySelector = document.getElementById('clearing-opportunities-table');
+  var template = `<tr><td>${name}</td><td>${grade}</td><td>${places}</td></tr>`;
+  opportunitySelector.innerHTML = opportunitySelector.innerHTML + template;
+}
